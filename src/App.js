26cad78b381eca976f4ea84b6f42c4cd8d1ec2a5
ulugbek <ulugbek@ulugbek.168.1.11>
2023-06-14 18:17:22 +0500

    `react-router-dom` has been added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
-function App() {
-  return (
-    <div className="w-full h-screen bg-black/[0.5] flex items-center justify-center flex-col gap-5 relative">
-      <h1 className="text-white text-6xl font-semibold tracking-wider uppercase">
-        Nefrit Academy
-      </h1>
-      <h1 className="text-white text-4xl font-semibold tracking-wider">
-        In Bukhara
-      </h1>
-      <p className="text-gray-400 text-3xl">coming soon...</p>
-      <div>
-        <div class="wave"></div>
-        <div class="wave"></div>
-        <div class="wave"></div>
-      </div>
-    </div>
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ComingSoon from "./pages/ComingSoon";
 
-    //  className="absolute left-0 top-0 w-full h-full bg-animation"
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ComingSoon />} />
+          <Route path="/about" element={<div>About</div>} />
+          <Route path="/contact" element={<div>Contact</div>} />
+        </Routes>
+      </Router>
+    </div>
   );
-}
+};
 
 export default App;
