c5c53774faa9bb62a06885c56ccb37d600d571d6
ulugbek <ulugbek@ulugbek.168.1.11>
2023-06-14 17:00:06 +0500

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-full h-screen bg-black/[0.5] flex items-center justify-center flex-col gap-5 relative">
+      <h1 className="text-white text-6xl font-semibold tracking-wider uppercase">
+        Nefrit Academy
+      </h1>
+      <h1 className="text-white text-4xl font-semibold tracking-wider">
+        In Bukhara
+      </h1>
+      <p className="text-gray-400 text-3xl">coming soon...</p>
+      <div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+        <div class="wave"></div>
+      </div>
     </div>
+
+    //  className="absolute left-0 top-0 w-full h-full bg-animation"
   );
 }
 
