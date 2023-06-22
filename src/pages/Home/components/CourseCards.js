df237e0811a016cd98ea7c96505e652d45460a2f
ulugbek <ulugbek@ulugbek.168.1.11>
2023-06-22 11:01:08 +0500

    overflow bug fixed

--- src/pages/Home/components/CourseCards.js
+++ src/pages/Home/components/CourseCards.js
@@ -10,8 +10,12 @@ import courses from "../../../db/courses.db";
 import Translate from "../../../utils/Translate";
 import { HiOutlineFolderOpen } from "react-icons/hi";
 import { MdOutlineCalendarMonth } from "react-icons/md";
+import { Link } from "react-router-dom";
+import { useSelector } from "react-redux";
 
 const CourseCards = () => {
+  const { language } = useSelector((state) => state.navbar);
+
   return (
     <div>
       <Swiper
@@ -19,26 +23,27 @@ const CourseCards = () => {
         grabCursor={true}
         modules={[EffectCards]}
         className="mySwiper"
+        initialSlide={Math.round(courses.length / 2)}
       >
         {courses.map(
           ({ image, name, description, modules, duration }, index) => {
             return (
               <SwiperSlide key={index} className={`bg-white dark:bg-gray-800`}>
-                <div className="p-8 h-full">
+                <div className="md:p-8 p-4 h-full">
                   <img
-                    className="w-full h-[270px] rounded-xl"
+                    className="w-full md:h-[270px] h-[150px] rounded-xl"
                     src={image}
                     alt={name.en}
                   />
-                  <div className="text-gray-900 dark:text-white py-5 flex flex-col justify-between h-[calc(100%-250px)]">
+                  <div className="text-gray-900 dark:text-white md:py-5 py-2 flex flex-col justify-between md:h-[calc(100%-250px)] h-[calc(100%-140px)]">
                     <div>
-                      <div className="flex items-center gap-10">
+                      <div className="flex items-center md:gap-10 gap-3">
                         <span className="flex items-center gap-2">
                           <HiOutlineFolderOpen
-                            size={24}
+                            size={window.innerWidth < 768 ? 18 : 24}
                             className="text-gray-600 dark:text-white/[0.5]"
                           />
-                          <h1 className="font-thin text-gray-600 dark:text-white/[0.6] pt-1">
+                          <h1 className="font-thin text-gray-600 md:text-md text-[16px] dark:text-white/[0.6] pt-1">
                             {modules}{" "}
                             <Translate
                               dictionary={{
@@ -51,10 +56,10 @@ const CourseCards = () => {
                         </span>
                         <span className="flex items-center gap-2">
                           <MdOutlineCalendarMonth
-                            size={24}
+                            size={window.innerWidth < 768 ? 18 : 24}
                             className="text-gray-600 dark:text-white/[0.5]"
                           />
-                          <h1 className="font-thin text-gray-600 dark:text-white/[0.6] pt-1">
+                          <h1 className="font-thin md:text-md text-[16px] text-gray-600 dark:text-white/[0.6] pt-1">
                             {duration}{" "}
                             <Translate
                               dictionary={{
@@ -66,16 +71,16 @@ const CourseCards = () => {
                           </h1>
                         </span>
                       </div>
-                      <h1 className="text-[28px] font-semibold my-2">
+                      <h1 className="md:text-[28px] font-semibold md:my-2 text-lg">
                         <Translate dictionary={name} />
                       </h1>
-                      <p className="text-lg leading-5 font-thin opacity-70">
+                      <p className="md:text-lg text-[12px] leading-5 font-thin opacity-70">
                         <Translate dictionary={description} />
                       </p>
                     </div>
-                    <button
-                      className="bg-blue-600 px-10 py-2 rounded-xl text-white"
-                      type=""
+                    <Link
+                      className="bg-blue-600 px-10 py-2 md:text-xl text-sm rounded-xl text-center text-white"
+                      to={language + "/contact"}
                     >
                       <Translate
                         dictionary={{
@@ -84,7 +89,7 @@ const CourseCards = () => {
                           en: "Learn more",
                         }}
                       />
-                    </button>
+                    </Link>
                   </div>
                 </div>
               </SwiperSlide>
